1289bf7ac67fa6fa90456c97a1700e69abdb274e
sam babu <158451994+sambabu17@users.noreply.github.com>
2024-05-15 18:56:45 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-      <div className="h-20 flex justify-between items-center border-t border-[#3A3A3A] bg-[#111111] w-full px-2 sm:px-[10vw] gap-10">
-        <h1 className="font-medium text-sm sm:text-md text-white">&copy; 2024 Samjove Babu . All rights reserved.</h1>
-        <h1 className="font-medium text-sm sm:text-md text-white">React Component Library</h1>
+      <div className="h-20 flex justify-between items-center border-t border-[#3A3A3A] bg-[#111111] w-full px-10 sm:px-[10vw] gap-10">
+        <h1 className="font-medium text-[10px] md:text-[14px] text-white w-1/2 ">&copy; 2024 Samjove Babu . All rights reserved.</h1>
+        <h1 className="font-medium text-[10px] md:text-[14px] text-white w-1/2 text-right ">Defix UI</h1>
       </div>
     );
   }
