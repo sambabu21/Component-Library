d82fad512ba8724b84349628c6cac31b60fc7ddf
sam babu <158451994+sambabu17@users.noreply.github.com>
2024-05-31 20:36:26 +0530

    effects

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -7,12 +7,13 @@ export default function Home() {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="py-40 w-full h-[calc(100vh-128px)] flex flex-col justify-center items-center bg-[#191919]" aria-label="Defix UI home page">
+    <div className="py-40 w-full h-[calc(100vh-128px)] flex flex-col justify-center items-center bg-[#191919]" aria-label="Defix UI home page relative">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_40%,black)]"></div>
       <div className="w-full md:w-[968px] flex flex-col justify-center items-start gap-10 sm:gap-20 md:pr-28 p-10 md:p-0">
         <motion.h1
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1, transition: { duration: 0.75 } }}
-          className=" text-4xl sm:text-6xl font-bold text-[#EEEEEE] sm:w-[80%]  sm:text-left tracking-tight"
+          className=" text-4xl sm:text-6xl font-bold text-[#EEEEEE] sm:w-[80%]  sm:text-left tracking-tight gradient-text"
         >
           Streamline your tailwind CSS projects
         </motion.h1>
